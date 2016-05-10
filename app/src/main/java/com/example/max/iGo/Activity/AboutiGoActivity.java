eb20b19484f60bc7a36a766d55564750574efda4
sdh <2503762598@qq.com>
2016-05-10 21:01:40 +0800

    aboutIgo

--- app/src/main/java/com/example/max/iGo/Activity/AboutiGoActivity.java
+++ app/src/main/java/com/example/max/iGo/Activity/AboutiGoActivity.java
@@ -16,7 +16,7 @@ import com.example.max.iGo.R;
 public class AboutiGoActivity extends BaseActivity {
 
     private TextView titleBarText;
-    private ImageView image_back;
+    private ImageView image_back;//aboutiGao
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
